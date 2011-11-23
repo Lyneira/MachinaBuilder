74bc6c2f7dfd125adec80d5da8db3bce02682074
Lyneira <lyneira@live.com>
2011-11-23 16:19:01 +0100

    Formatting style change

--- src/me/lyneira/MachinaBuilder/Blueprint.java
+++ src/me/lyneira/MachinaBuilder/Blueprint.java
@@ -23,131 +23,114 @@ import me.lyneira.MachinaCraft.MovableBlueprint;
  * @author Lyneira
  */
 public class Blueprint extends MovableBlueprint {
-	private static List<BlueprintFactory> blueprints;
-	final static int mainModuleIndex;
-	final static int leftModuleIndex;
-	final static int rightModuleIndex;
+    private static List<BlueprintFactory> blueprints;
+    final static int mainModuleIndex;
+    final static int leftModuleIndex;
+    final static int rightModuleIndex;
 
-	final static Material headMaterial = Material.IRON_BLOCK;
-	private final static Material baseMaterial = Material.WOOD;
-	private final static Material furnaceMaterial = Material.FURNACE;
-	private final static Material burningFurnaceMaterial = Material.BURNING_FURNACE;
-	private final static Material supplyContainerMaterial = Material.CHEST;
-	final static Material rotateMaterial = Material.STICK;
+    final static Material headMaterial = Material.IRON_BLOCK;
+    private final static Material baseMaterial = Material.WOOD;
+    private final static Material furnaceMaterial = Material.FURNACE;
+    private final static Material burningFurnaceMaterial = Material.BURNING_FURNACE;
+    private final static Material supplyContainerMaterial = Material.CHEST;
+    final static Material rotateMaterial = Material.STICK;
 
-	final static int leverIndex;
-	final static int centralBaseIndex;
-	final static int furnaceIndex;
-	final static int containerIndex;
-	final static int primaryHeadIndex;
+    final static int leverIndex;
+    final static int centralBaseIndex;
+    final static int furnaceIndex;
+    final static int containerIndex;
+    final static int primaryHeadIndex;
 
-	final static int leftHeadIndex;
-	final static int rightHeadIndex;
+    final static int leftHeadIndex;
+    final static int rightHeadIndex;
 
-	static {
-		blueprints = new ArrayList<BlueprintFactory>(3);
+    static {
+        blueprints = new ArrayList<BlueprintFactory>(3);
 
-		mainModuleIndex = blueprints.size();
-		BlueprintFactory mainModule = new BlueprintFactory();
-		blueprints.add(mainModule);
+        mainModuleIndex = blueprints.size();
+        BlueprintFactory mainModule = new BlueprintFactory();
+        blueprints.add(mainModule);
 
-		leftModuleIndex = blueprints.size();
-		BlueprintFactory leftModule = new BlueprintFactory();
-		blueprints.add(leftModule);
+        leftModuleIndex = blueprints.size();
+        BlueprintFactory leftModule = new BlueprintFactory();
+        blueprints.add(leftModule);
 
-		rightModuleIndex = blueprints.size();
-		BlueprintFactory rightModule = new BlueprintFactory();
-		blueprints.add(rightModule);
+        rightModuleIndex = blueprints.size();
+        BlueprintFactory rightModule = new BlueprintFactory();
+        blueprints.add(rightModule);
 
-		mainModule.addKey(new BlockVector(0, 1, 0), Material.LEVER)
-				.addKey(new BlockVector(0, 0, 0), baseMaterial)
-				.addKey(new BlockVector(-1, 0, 0), burningFurnaceMaterial)
-				.addKey(new BlockVector(1, 1, 0), supplyContainerMaterial)
-				.addKey(new BlockVector(1, 0, 0), headMaterial);
+        mainModule.addKey(new BlockVector(0, 1, 0), Material.LEVER).addKey(new BlockVector(0, 0, 0), baseMaterial).addKey(new BlockVector(-1, 0, 0), burningFurnaceMaterial)
+                .addKey(new BlockVector(1, 1, 0), supplyContainerMaterial).addKey(new BlockVector(1, 0, 0), headMaterial);
 
-		leftModule.addKey(new BlockVector(1, 0, -1), headMaterial).add(
-				new BlockVector(0, 0, -1), baseMaterial);
+        leftModule.addKey(new BlockVector(1, 0, -1), headMaterial).add(new BlockVector(0, 0, -1), baseMaterial);
 
-		rightModule.addKey(new BlockVector(1, 0, 1), headMaterial).add(
-				new BlockVector(0, 0, 1), baseMaterial);
+        rightModule.addKey(new BlockVector(1, 0, 1), headMaterial).add(new BlockVector(0, 0, 1), baseMaterial);
 
-		// Get handles to key blocks now. This finalizes the blueprints.
-		ListIterator<Integer> handles = mainModule.getHandlesFinal()
-				.listIterator();
-		leverIndex = handles.next();
-		centralBaseIndex = handles.next();
-		furnaceIndex = handles.next();
-		containerIndex = handles.next();
-		primaryHeadIndex = handles.next();
-		handles = leftModule.getHandlesFinal().listIterator();
-		leftHeadIndex = handles.next();
-		handles = rightModule.getHandlesFinal().listIterator();
-		rightHeadIndex = handles.next();
-	}
+        // Get handles to key blocks now. This finalizes the blueprints.
+        ListIterator<Integer> handles = mainModule.getHandlesFinal().listIterator();
+        leverIndex = handles.next();
+        centralBaseIndex = handles.next();
+        furnaceIndex = handles.next();
+        containerIndex = handles.next();
+        primaryHeadIndex = handles.next();
+        handles = leftModule.getHandlesFinal().listIterator();
+        leftHeadIndex = handles.next();
+        handles = rightModule.getHandlesFinal().listIterator();
+        rightHeadIndex = handles.next();
+    }
 
-	public final static Blueprint instance = new Blueprint();
+    public final static Blueprint instance = new Blueprint();
 
-	private Blueprint() {
-		super(blueprints);
-		blueprints = null;
-	}
+    private Blueprint() {
+        super(blueprints);
+        blueprints = null;
+    }
 
-	/**
-	 * Detects whether a builder is present at the given BlockLocation. Key
-	 * blocks defined above must be detected manually.
-	 */
-	public Machina detect(Player player, BlockLocation anchor,
-			BlockFace leverFace, ItemStack itemInHand) {
-		if (leverFace != BlockFace.UP)
-			return null;
+    /**
+     * Detects whether a builder is present at the given BlockLocation. Key
+     * blocks defined above must be detected manually.
+     */
+    public Machina detect(Player player, BlockLocation anchor, BlockFace leverFace, ItemStack itemInHand) {
+        if (leverFace != BlockFace.UP)
+            return null;
 
-		if (!player.hasPermission("machinabuilder.activate"))
-			return null;
+        if (!player.hasPermission("machinabuilder.activate"))
+            return null;
 
-		// Check if the Builder is on solid ground.
-		if (!BlockData.isSolid(anchor.getRelative(BlockFace.DOWN).getTypeId()))
-			return null;
+        // Check if the Builder is on solid ground.
+        if (!BlockData.isSolid(anchor.getRelative(BlockFace.DOWN).getTypeId()))
+            return null;
 
-		if (anchor.checkType(baseMaterial)) {
-			// Search for a furnace around the anchor.
-			for (BlockRotation i : BlockRotation.values()) {
-				if (anchor.getRelative(i.getYawFace()).checkType(
-						furnaceMaterial)) {
-					BlockRotation yaw = i.getOpposite();
-					BlockLocation primaryHead = anchor.getRelative(yaw
-							.getYawFace());
-					if (primaryHead.checkType(headMaterial)
-							&& primaryHead.getRelative(BlockFace.UP).checkType(
-									supplyContainerMaterial)) {
-						List<Integer> detectedModules = new ArrayList<Integer>(
-								3);
-						detectedModules.add(mainModuleIndex);
-						// Detect optional modules here.
-						BlockLocation head = primaryHead.getRelative(yaw
-								.getLeft().getYawFace());
-						if (head.checkType(headMaterial)
-								&& detectOther(anchor, yaw, leftModuleIndex)) {
-							detectedModules.add(leftModuleIndex);
-						}
-						head = primaryHead.getRelative(yaw.getRight()
-								.getYawFace());
-						if (head.checkType(headMaterial)
-								&& detectOther(anchor, yaw, rightModuleIndex)) {
-							detectedModules.add(rightModuleIndex);
-						}
-						Builder builder = new Builder(instance, detectedModules, yaw, player, anchor);
-						if (itemInHand != null
-								&& itemInHand.getType() == rotateMaterial) {
-							builder.doRotate(anchor, BlockRotation.yawFromLocation(player.getLocation()));
-							builder.onDeActivate(anchor);
-							builder = null;
-						}
-						return builder;
-					}
-				}
-			}
-		}
-		return null;
-	}
+        if (anchor.checkType(baseMaterial)) {
+            // Search for a furnace around the anchor.
+            for (BlockRotation i : BlockRotation.values()) {
+                if (anchor.getRelative(i.getYawFace()).checkType(furnaceMaterial)) {
+                    BlockRotation yaw = i.getOpposite();
+                    BlockLocation primaryHead = anchor.getRelative(yaw.getYawFace());
+                    if (primaryHead.checkType(headMaterial) && primaryHead.getRelative(BlockFace.UP).checkType(supplyContainerMaterial)) {
+                        List<Integer> detectedModules = new ArrayList<Integer>(3);
+                        detectedModules.add(mainModuleIndex);
+                        // Detect optional modules here.
+                        BlockLocation head = primaryHead.getRelative(yaw.getLeft().getYawFace());
+                        if (head.checkType(headMaterial) && detectOther(anchor, yaw, leftModuleIndex)) {
+                            detectedModules.add(leftModuleIndex);
+                        }
+                        head = primaryHead.getRelative(yaw.getRight().getYawFace());
+                        if (head.checkType(headMaterial) && detectOther(anchor, yaw, rightModuleIndex)) {
+                            detectedModules.add(rightModuleIndex);
+                        }
+                        Builder builder = new Builder(instance, detectedModules, yaw, player, anchor);
+                        if (itemInHand != null && itemInHand.getType() == rotateMaterial) {
+                            builder.doRotate(anchor, BlockRotation.yawFromLocation(player.getLocation()));
+                            builder.onDeActivate(anchor);
+                            builder = null;
+                        }
+                        return builder;
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
 }
